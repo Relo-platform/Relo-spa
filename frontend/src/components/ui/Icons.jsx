19151ecea7ef780f08ef72dcb475b935f0c56b1f
railsenjoyer <railsenjoyer@gmail.com>
2026-06-20 22:53:17 +0300

    adjust general styles & colors

--- frontend/src/components/ui/Icons.jsx
+++ frontend/src/components/ui/Icons.jsx
@@ -1,4 +1,4 @@
-export const CheckmarkIcon = ({size = 24, color = "var(--color-green-bg-button-)"}) => (
+export const CheckmarkIcon = ({size = 24, color = "var(--color-green-bg-button)"}) => (
   <svg width={size} height={size} viewBox="0 0 24 24" fill="none" stroke={color} strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round">
     <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path>
     <polyline points="22 4 12 14.01 9 11.01"></polyline>
@@ -62,3 +62,10 @@ export const Stars = ({ rating, size = 18 }) => {
   );
 };
 
+export const CloseIcon = () => {
+  return (
+    <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M13 1L1 13M1 1L13 13" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+    </svg>
+  );
+};
